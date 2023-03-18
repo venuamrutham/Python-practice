e1c25cb680f5b97dbfbefd9eeefe4f1f31a1ecf7
Amrutham venu <115334303+venuamrutham@users.noreply.github.com>
2023-03-18 12:54:10 +0530

    Add files via upload

---
+++ # Multiplication table.py
@@ -0,0 +1,21 @@
+# Multiplication table
+
+a = int(input("Please enter a integer to display table of it:  "))
+
+for i in range(1,11):
+
+    multipication = a*i
+
+    print(i,"X",a, "=",multipication )
+
+
+
+
+
+
+
+
+
+
+
+
