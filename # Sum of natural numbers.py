db9bd6a0c0a18a7ddb5e900a95d5b73a851493b7
Amrutham venu <115334303+venuamrutham@users.noreply.github.com>
2023-03-18 12:59:11 +0530

    Add files via upload

---
+++ # Sum of natural numbers.py
@@ -0,0 +1,21 @@
+# Sum of natural numbers
+
+a = int(input("Please enter a integer to display table of it:  "))
+
+for i in range(1,11):
+
+    multipication = a*i
+
+    print(i,"X",a, "=",multipication )
+
+
+
+
+
+
+
+
+
+
+
+
