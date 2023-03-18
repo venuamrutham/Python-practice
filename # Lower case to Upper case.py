61efb2e7a8720e9058449b39b527e95ac672487c
Amrutham venu <115334303+venuamrutham@users.noreply.github.com>
2023-03-18 13:48:30 +0530

    Add files via upload

---
+++ # Lower case to Upper case.py
@@ -0,0 +1,10 @@
+
+# Lower case to Upper case 
+
+a = input("Enter a word to convert into uppercase: ")
+
+a = a.upper()
+
+print(a, "is after conversion")
+
+
---
+++ # Upper case to Lower case.py
@@ -0,0 +1,10 @@
+
+# Upper case to Lower case
+
+a = input("Enter a word to convert into lowercase: ")
+
+a = a.lower()
+
+print(a, "is after conversion")
+
+
